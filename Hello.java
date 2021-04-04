a0f2ebd308dc20550d5a71641f852b0aed38bb05
virantirs <virantirahmah4@gmail.com>
2021-04-04 17:35:40 +0800

    Hello Git & Java

--- Hello.java
+++ Hello.java
@@ -1,5 +1,6 @@
 public class Hello{
  public static void main (String [] ags){
-  System.out.println("Hello World !!");
+  System.out.println("Hello Git !!");
+  System.out.println("Hello Java !!");
  }
 }
